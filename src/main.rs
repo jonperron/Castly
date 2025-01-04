0eb96587f53c4d1a94a5af3ef525704f860b18b5
Jonathan Perron <jonathan.perron@lumapps.com>
2025-01-04 11:18:52 +0100

    fix(review): improve code quality
    Changelog:
    * Remove useless cargo from cargo.toml
    * Remove useless async_trait since it's handled in rust now
    * Simplify dependancies to X.Y
    * Add dev-dependancies section in cargo.toml
    * Add AppState to handle better shared resources (template engine...)
    * Remove provider from configuration
    * Add enum to check provider validity

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use axum::{routing::get, Router};
 use tracing::{info, Level};
 use tracing_subscriber;
 
-use crate::api::send_router;
+use crate::api::{send_router, AppState};
 use crate::config::Config;
 use crate::providers::MailgunProvider;
 use crate::templates_engines::tera_engine::TemplateEngine;
@@ -47,13 +47,14 @@ async fn main() {
             .expect("Mailgun config is missing"),
     ));
 
-    let app = send_router(template_engine, mailgun_provider)
-        .merge(Router::new().route("/health", get(health_handler)));
+    let state = AppState {
+        template_engine,
+        mailgun_provider,
+    };
+
+    let app = send_router(state).merge(Router::new().route("/health", get(health_handler)));
 
     let addr: SocketAddr = format!("0.0.0.0:{}", config.service.port).parse().unwrap();
-    if config.service.environment == "development" {
-        println!("Listening on http://{}", addr);
-    }
 
     let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
 
